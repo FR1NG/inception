2f5464e8d2e1725ac5be7beab801840a313be244
hamza chakoubi <hamza.chakoubi28@gmail.com>
2022-11-27 04:59:10 -0500

    database problem fixed

--- srcs/requirements/mysql/tools/init.sh
+++ srcs/requirements/mysql/tools/init.sh
@@ -4,6 +4,7 @@ service mysql start
 mysql -u root  << EOF
     CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
     CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-    GRANT ALL PRIVILEGES ON wpdbname.* TO '${DB_USER}'@'%';
+    GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
     FLUSH PRIVILEGES;
 EOF
